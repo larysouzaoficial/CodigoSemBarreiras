c648d7f743db9429a6f77ead110afec49ceaaee1
Larissa Souza <lau_saj@hotmail.com>
2021-08-31 21:48:20 -0300

    Arquivo teste 2

--- src/Assunto3/arquivoTeste.java
+++ src/Assunto3/arquivoTeste.java
@@ -1,6 +1,6 @@
 package Assunto3;
    public class arquivoTeste {
        public static void main(String[] args) {
-           System.out.println("hello");
+           System.out.println("olá");
        }
 }
